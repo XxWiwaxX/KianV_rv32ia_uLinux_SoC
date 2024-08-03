6d1aa0d3a76aa8d6dfa13a95e659c5e3124206ee
Hirosh Dabui <hirosh@dabui.de>
2024-08-03 11:03:25 +0200

    Improve QSPI timing: Add setup time for flash memory

--- src/qqspi.v
+++ src/qqspi.v
@@ -107,7 +107,7 @@ module qqspi #(parameter CHIP_SELECTS = 3)
   always @(posedge clk) begin
     if (!resetn) begin
       ce <= ~0;
-      sclk <= 1'b0;
+      sclk <= 1'b1;
       sio_oe <= 4'b0000;
       sio_out <= 4'b0000;
       spi_buf <= 0;
